58a1875edc1a18e79e552efba06bca8847322ed4
Jinwu Yang <131998405+jinwuyang@users.noreply.github.com>
2026-04-07 02:31:43 +0800

    ENEC_ISCA_AE

--- data_prepare.sh
+++ data_prepare.sh
@@ -4,6 +4,7 @@
 echo "Running download_data.sh..."
 bash download_data.sh
 
+pip install transformers
 # 2. Run Python splitting/utility script
 echo "Running python/utils.py..."
 python3 python/utils.py
